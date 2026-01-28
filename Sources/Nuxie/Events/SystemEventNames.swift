febecf574fd780a8c88e095c7a000d45a910e0c3
Levi McCallum <levi@levimccallum.com>
2026-01-28 13:54:11 -0800

    feat: unify system event triggers

---
+++ Sources/Nuxie/Events/SystemEventNames.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+enum SystemEventNames {
+    static let screenShown = "$screen_shown"
+    static let screenDismissed = "$screen_dismissed"
+    static let flowEntered = "$flow_entered"
+    static let purchaseCompleted = "$purchase_completed"
+    static let purchaseFailed = "$purchase_failed"
+    static let purchaseCancelled = "$purchase_cancelled"
+    static let restoreCompleted = "$restore_completed"
+    static let restoreFailed = "$restore_failed"
+    static let restoreNoPurchases = "$restore_no_purchases"
+}
