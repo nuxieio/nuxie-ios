88c542c74ad2de9b585d66687bbabb804edd64ca
Levi McCallum <levi@levimccallum.com>
2026-01-28 14:49:57 -0800

    feat: rename set_value trigger to did_set

--- Sources/Nuxie/Flows/FlowViewController.swift
+++ Sources/Nuxie/Flows/FlowViewController.swift
@@ -340,7 +340,7 @@ extension FlowViewController {
             runtimeReady = true
             runtimeDelegate?.flowViewController(self, didReceiveRuntimeMessage: type, payload: payload, id: id)
             flushPendingRuntimeMessages()
-        case "runtime/screen_changed", "action/set_value", "action/event":
+        case "runtime/screen_changed", "action/did_set", "action/event":
             runtimeDelegate?.flowViewController(self, didReceiveRuntimeMessage: type, payload: payload, id: id)
         case "action/purchase":
             if runtimeDelegate != nil {
